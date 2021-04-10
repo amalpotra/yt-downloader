e0394848a7ae61cb8978ca559f4848103c4ffb59
amarjeetmalpotra <amarjeetmalpotra@outlook.com>
2021-04-10 14:36:14 +0530

    Added more widgets - added message box support to show warnings - added progress bar and download buttons - setup a seperate download thread - fixed UI crash because of thumbnail loading timeout - adjusted various styling

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import sys, socket, time
 from pytube import YouTube
-from PyQt6.QtWidgets import QApplication, QStatusBar, QWidget, QLabel, QLineEdit, QPushButton, QTextEdit, QVBoxLayout, QHBoxLayout
+from PyQt6.QtWidgets import QApplication, QStatusBar, QWidget, QLabel, QLineEdit, QPushButton, QProgressBar, QComboBox, QMessageBox, QVBoxLayout, QHBoxLayout
 from PyQt6.QtGui import QIcon, QPixmap
-from PyQt6.QtCore import QThread, pyqtSignal
+from PyQt6.QtCore import Qt, QThread, pyqtSignal
 from urllib.request import urlopen
 from datetime import timedelta
 
@@ -12,23 +12,23 @@ class ConnectionThread(QThread):
         while True:
             try:
                 # check if the host is reachable
-                con = socket.create_connection(("8.8.8.8", 53))
+                con = socket.create_connection(('8.8.8.8', 53))
                 con.close()
                 # emit available signal
                 self.con_response.emit(True)
-                continue
             except OSError:
-                pass
+                # emit unavailable signal
+                self.con_response.emit(False)
             finally:
                 # sleep for 3 seconds just to avoid overhead
                 time.sleep(3)
-            # emit unavailable signal
-            self.con_response.emit(False)
 
 # seperate worker thread for background processing and to avoid UI freez 
 class WorkerThread(QThread):
     # setup response signal
     worker_response = pyqtSignal(tuple)
+    # setup error signal
+    worker_err_response = pyqtSignal()
     # additional parameter as url
     def __init__(self, url):
         # invoke the __init__ of super as well
@@ -37,13 +37,32 @@ class WorkerThread(QThread):
     def run(self):
         try:
             yt = YouTube(self.url)
+            # load thumbnail image
+            pixmap = QPixmap()
+            pixmap.loadFromData(urlopen(str(yt.thumbnail_url)).read())
             # emitting the response signal
-            # self.worker_response.emit(str(yt.streams.filter(progressive='true', file_extension='mp4')))
-            self.worker_response.emit((yt.thumbnail_url, yt.title, yt.author, yt.length, yt.publish_date))
-        except Exception as ex:
-            pass
-            # emitting the response signal
-            # self.worker_response.emit('Enter valid url!','','','','')
+            self.worker_response.emit((
+                pixmap,
+                yt.title,
+                yt.author,
+                yt.length,
+                yt.publish_date,
+                # populate a list of progressive mp4 resolutions for the download options
+                [res.resolution for res in yt.streams.filter(progressive='true', file_extension='mp4').order_by('resolution')]
+            ))
+        except:
+            # emitting the error signal
+            self.worker_err_response.emit()
+
+# download thread
+class DownloadThread(QThread):
+    # setup download signal
+    download_response = pyqtSignal(str)
+    def __init__(self, download_type):
+        super(DownloadThread, self).__init__()
+        self.download_type = download_type
+    def run(self):
+        self.download_response.emit(self.download_type)
 
 class YTdownloader(QWidget):
     def __init__(self):
@@ -51,10 +70,11 @@ class YTdownloader(QWidget):
         # setup some window specific things
         self.setWindowTitle('Youtube Downloader')
         self.setWindowIcon(QIcon('assets/yt-icon.ico'))
-        self.resize(700, 400)
+        self.setFixedSize(705, 334)
 
         # parent layout
         layout = QVBoxLayout()
+        layout.setContentsMargins(15, 15, 15, 10)
         self.setLayout(layout)
 
         # top bar layout
@@ -64,48 +84,84 @@ class YTdownloader(QWidget):
         detailSec = QHBoxLayout()
         metaSec = QVBoxLayout()
 
+        # download section
+        downloadSec = QHBoxLayout()
+        downloadBtn = QVBoxLayout()
+
         # status bar
         self.statusBar = QStatusBar()
 
-        # widgets
+        # message box
+        self.message = QMessageBox()
+
+        # setting up widgets
         urlLabel = QLabel('URL: ')
         self.urlBox = QLineEdit()
-        button = QPushButton('&Get')
-        button.clicked.connect(self.getDetails)
-        self.output = QTextEdit()
+        self.urlBox.setFocusPolicy(Qt.FocusPolicy.ClickFocus or Qt.FocusPolicy.NoFocus)
+        self.urlBox.setPlaceholderText('Enter or paste video URL...')
+        self.button = QPushButton('Get')
+        self.button.setDefault(True)
+        self.button.clicked.connect(self.getDetails)
 
+        # thumbnail
         pixmap = QPixmap('assets\placeholder.jpg')
         self.thumb = QLabel()
         self.thumb.setFixedSize(250, 141)
         self.thumb.setScaledContents(True)
         self.thumb.setPixmap(pixmap)
 
+        # detail widgets
         self.title = QLabel('Title: ')
         self.author = QLabel('Author: ')
         self.length = QLabel('Duration: ')
         self.publish_date = QLabel('Published: ')
 
+        # progress bar
+        self.progress_bar = QProgressBar()
+        
+        # download options
+        self.download = QComboBox()
+        self.download.setPlaceholderText('Download video')
+        self.download.activated.connect(self.getVideo)
+        self.download.setEnabled(False)
+
+        # download audio button
+        self.download_audio = QPushButton('Download audio')
+        self.download_audio.clicked.connect(self.getAudio)
+        self.download_audio.setEnabled(False)
+
         # add widgets and layouts
         topBar.addWidget(urlLabel)
         topBar.addWidget(self.urlBox)
-        topBar.addWidget(button)
+        topBar.addWidget(self.button)
 
+        # detail section
         metaSec.addWidget(self.title)
         metaSec.addWidget(self.author)
         metaSec.addWidget(self.length)
         metaSec.addWidget(self.publish_date)
         detailSec.addWidget(self.thumb)
+        detailSec.addSpacing(20)
         detailSec.addLayout(metaSec)
+
+        # download section
+        downloadBtn.addWidget(self.download)
+        downloadBtn.addWidget(self.download_audio)
+        downloadSec.addWidget(self.progress_bar)
+        downloadSec.addLayout(downloadBtn)
 
         # add content to parent layout
         layout.addLayout(topBar)
+        layout.addSpacing(20)
         layout.addLayout(detailSec)
-        layout.addWidget(self.output)
+        layout.addSpacing(5)
+        layout.addLayout(downloadSec)
         layout.addWidget(self.statusBar)
 
         # setup a connection thread to keep checking internet connectivity
         self.connection = ConnectionThread()
         self.connection.start()
+
         # catch the connection response signal
         self.connection.con_response.connect(self.connection_slot)
 
@@ -115,26 +171,53 @@ class YTdownloader(QWidget):
         curMsg = self.statusBar.currentMessage()
         # connection succeeded
         if status:
-            if curMsg == '🔴 Disconnected':
-                self.statusBar.showMessage('🟢 Connection restored!', 3000)
-            elif curMsg != '🟢 Connected':
-                self.statusBar.showMessage('🟢 Connected')
+            if curMsg == '🔴  Disconnected':
+                self.statusBar.showMessage('🟢  Connection restored!', 3000)
+            elif curMsg != '🟢  Connected':
+                self.statusBar.showMessage('🟢  Connected')
         # connection failed
-        elif curMsg == '🟢 Connected':
-            self.statusBar.showMessage('🔴 Connection interrupted!', 3000)
-        elif curMsg != '🔴 Disconnected': 
-            self.statusBar.showMessage('🔴 Disconnected')
+        elif curMsg == '🟢  Connected':
+            self.statusBar.showMessage('🔴  Connection interrupted!', 3000)
+        elif curMsg != '🔴  Disconnected': 
+            self.statusBar.showMessage('🔴  Disconnected')
 
     # get button slot
     def getDetails(self):
-        # setup a worker thread to keep UI responsive
-        self.worker = WorkerThread(self.urlBox.text())
-        self.worker.start()
-        # catch the finished signal
-        self.worker.finished.connect(self.finished_slot)
-        # catch the response signal
-        self.worker.worker_response.connect(self.response_slot)
-        self.output.setText('Button clicked !')
+        curMsg = self.statusBar.currentMessage()
+        if curMsg == '🔴  Disconnected' or curMsg == '🔴  Connection interrupted!':
+            self.message.critical(
+                self,
+                'Error',
+                'Conection failed!\nAre you sure you\'re connected to internet ? '
+            )
+        elif self.button.text() == 'Get':
+            self.button.setText('Fetching...')
+            self.button.setDisabled(True)
+            # setup a worker thread to keep UI responsive
+            self.worker = WorkerThread(self.urlBox.text())
+            self.worker.start()
+            # catch the finished signal
+            self.worker.finished.connect(self.finished_slot)
+            # catch the response signal
+            self.worker.worker_response.connect(self.response_slot)
+            # catch the error signal
+            self.worker.worker_err_response.connect(self.err_slot)
+
+    # download video combo box slot
+    def getVideo(self):
+        self.message.critical(
+                self,
+                'Error',
+                f'video got triggered {self.download.currentText()}'
+        )
+
+    # download audio button slot
+    def getAudio(self):
+        self.message.critical(
+                self,
+                'Error',
+                'Audio got triggered'
+        )
     
     # finished slot
     def finished_slot(self):
@@ -142,20 +225,40 @@ class YTdownloader(QWidget):
 
     # response slot
     def response_slot(self, res):
-        pixmap = QPixmap()
-        pixmap.loadFromData(urlopen(str(res[0])).read())
-        self.thumb.setPixmap(pixmap)
+        # set back the button text and enable it
+        self.button.setText('Get')
+        self.button.setDisabled(False)
+        # set the actual thumbnail of requested video
+        self.thumb.setPixmap(res[0])
+        # slice the title if it is more than the limit
         if len(res[1]) > 50:
             self.title.setText(f'Title:  {res[1][:50]}...')
         else:
             self.title.setText(f'Title:  {res[1]}')
+        # set leftover details
         self.author.setText(f'Author:  {res[2]}')
         self.length.setText('Duration:  {}'.format(str(timedelta(seconds=res[3]))))
         self.publish_date.setText(f'Published:  {res[4].strftime("%m/%d/%Y")}')
-        #self.output.setText(res)
+        # clear any previous items if any
+        self.download.clear()
+        # add resolutions as items to the download button and enable them
+        self.download.addItems(['MP4 - ' + res for res in res[5]])
+        self.download.setDisabled(False)
+        self.download_audio.setDisabled(False)
+
+    # error slot
+    def err_slot(self):
+        # show the warning message
+        self.message.warning(
+            self,
+            'Warning',
+            'Something went wrong!\nProbably a broken link or some restricted content... '
+        )
+        # set back the button text and enable it
+        self.button.setText('Get')
+        self.button.setDisabled(False)
 
 if __name__ == '__main__':
-    # app = QApplication([])
     # instantiate the application
     app = QApplication(sys.argv)
     # setup a custom styleSheet
@@ -165,6 +268,27 @@ if __name__ == '__main__':
         }
         QStatusBar {
             font-size: 12px;
+        }
+        QLineEdit {
+            border: none;
+            border-bottom: 2px solid #808080;
+            padding: 2px 5px;
+            margin: 0 10px;
+            background: transparent;
+        }
+        QPushButton {
+            width: 125px;
+            padding: 5.5px 0;
+        }
+        QComboBox {
+            padding: 3px 30px 3px 45px;
+        }
+        QMessageBox QLabel {
+            font-size: 13px;
+        }
+        QMessageBox QPushButton {
+            width: auto;
+            padding: 3px 20px;
         }
     ''')
     window = YTdownloader()
